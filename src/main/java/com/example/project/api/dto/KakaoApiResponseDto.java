f3b61dc94095ccd71eca2f9f4eb9a270dd559869
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-23 15:11:02 +0900

    feat: implement service related kakao address search api

---
+++ src/main/java/com/example/project/api/dto/KakaoApiResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.project.api.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KakaoApiResponseDto {
+
+	private MetaDto metaDto;
+	private List<DocumentDto> documentList;
+}
