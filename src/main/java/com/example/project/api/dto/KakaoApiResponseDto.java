b93e1266e60a85966c12f7689ab936d25b73744f
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-23 15:12:11 +0900

    feat: add missing json property annotation

--- src/main/java/com/example/project/api/dto/KakaoApiResponseDto.java
+++ src/main/java/com/example/project/api/dto/KakaoApiResponseDto.java
@@ -2,6 +2,8 @@ package com.example.project.api.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class KakaoApiResponseDto {
 
+	@JsonProperty("meta")
 	private MetaDto metaDto;
+
+	@JsonProperty("documents")
 	private List<DocumentDto> documentList;
 }
