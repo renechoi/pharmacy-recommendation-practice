f7f8dc7de70afc0c39d88b341e8e0f6289c5ac87
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-29 21:54:22 +0900

    feat: bootstrap, handlebars를 이용한 뷰 구현 및 테스트 작성

---
+++ src/main/java/com/example/project/direction/controller/FormController.java
@@ -0,0 +1,36 @@
+package com.example.project.direction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.project.direction.dto.InputDto;
+import com.example.project.pharmacy.service.PharmacyRecommendationService;
+
+@Controller
+@RequiredArgsConstructor
+public class FormController {
+	private final PharmacyRecommendationService pharmacyRecommendationService;
+
+	@GetMapping("/")
+	public String main() {
+		return "main";
+	}
+
+
+	@PostMapping("/search")
+	public ModelAndView postDirection(@ModelAttribute InputDto inputDto)  {
+
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("output");
+		modelAndView.addObject("outputFormList",
+			pharmacyRecommendationService.recommendPharmacyList(inputDto.getAddress()));
+
+		return modelAndView;
+	}
+
+
+}
--- src/main/java/com/example/project/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/project/pharmacy/service/PharmacyRecommendationService.java
@@ -1,20 +1,22 @@
 package com.example.project.pharmacy.service;
 
-import java.util.List;
-import java.util.Objects;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import com.example.project.api.dto.DocumentDto;
 import com.example.project.api.dto.KakaoApiResponseDto;
 import com.example.project.api.service.KakaoAddressSearchService;
+import com.example.project.direction.dto.OutputDto;
 import com.example.project.direction.entity.Direction;
 import com.example.project.direction.service.DirectionService;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
@@ -29,13 +31,13 @@ public class PharmacyRecommendationService {
 	@Value("${pharmacy.recommendation.base.url}")
 	private String baseUrl;
 
-	public void recommendPharmacyList(String address) {
+	public List<OutputDto> recommendPharmacyList(String address) {
 
 		KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
 		if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
 			log.error("[PharmacyRecommendationService.recommendPharmacyList fail] Input address: {}", address);
-			return;
+			return Collections.emptyList();
 		}
 
 		DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
@@ -43,6 +45,20 @@ public class PharmacyRecommendationService {
 		// List<Direction> directionList = directionService.buildDirectionList(documentDto);
 		List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
 
-		directionService.saveAll(directionList);
+		return directionService.saveAll(directionList)
+			.stream()
+			.map(this::convertToOutputDto)
+			.collect(Collectors.toList());
+	}
+
+	private OutputDto convertToOutputDto(Direction direction) {
+
+		return OutputDto.builder()
+			.pharmacyName(direction.getTargetPharmacyName())
+			.pharmacyAddress(direction.getTargetAddress())
+			.directionUrl(baseUrl)
+			.roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
+			.distance(String.format("%.2f km", direction.getDistance()))
+			.build();
 	}
 }
