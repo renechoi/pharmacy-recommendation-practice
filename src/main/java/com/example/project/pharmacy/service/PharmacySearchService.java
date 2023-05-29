640cef21745285efc3d5d8461cd8777d49517b34
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-30 00:13:30 +0900

    feat: redis template 적용 코드 작성

--- src/main/java/com/example/project/pharmacy/service/PharmacySearchService.java
+++ src/main/java/com/example/project/pharmacy/service/PharmacySearchService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import com.example.project.pharmacy.cache.PharmacyRedisTemplateService;
 import com.example.project.pharmacy.dto.PharmacyDto;
 import com.example.project.pharmacy.entity.Pharmacy;
 
@@ -16,27 +18,31 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class PharmacySearchService {
 
-    private final PharmacyRepositoryService pharmacyRepositoryService;
+	private final PharmacyRepositoryService pharmacyRepositoryService;
+	private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
 
-    public List<PharmacyDto> searchPharmacyDtoList() {
+	public List<PharmacyDto> searchPharmacyDtoList() {
 
-        // redis
+		// redis
+		List<PharmacyDto> pharmacyDtoList = pharmacyRedisTemplateService.findAll();
+		if (!CollectionUtils.isEmpty(pharmacyDtoList))
+			return pharmacyDtoList;
 
-        // db
-        return pharmacyRepositoryService.findAll()
-                .stream()
-                .map(this::convertToPharmacyDto)
-                .collect(Collectors.toList());
-    }
+		// db
+		return pharmacyRepositoryService.findAll()
+			.stream()
+			.map(this::convertToPharmacyDto)
+			.collect(Collectors.toList());
+	}
 
-    private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy) {
+	private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy) {
 
-        return PharmacyDto.builder()
-                .id(pharmacy.getId())
-                .pharmacyName(pharmacy.getPharmacyName())
-                .pharmacyAddress(pharmacy.getPharmacyAddress())
-                .latitude(pharmacy.getLatitude())
-                .longitude(pharmacy.getLongitude())
-                .build();
-    }
+		return PharmacyDto.builder()
+			.id(pharmacy.getId())
+			.pharmacyName(pharmacy.getPharmacyName())
+			.pharmacyAddress(pharmacy.getPharmacyAddress())
+			.latitude(pharmacy.getLatitude())
+			.longitude(pharmacy.getLongitude())
+			.build();
+	}
 }
